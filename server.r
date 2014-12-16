d90606f38fc50ae29204fe3c708a33a2a4f6d817
manuelblancovalentin <wildsoul@hotmail.es>
2014-12-16 18:29:10 -0200

    v4.0
    Colors

--- server.r
+++ server.r
@@ -10,17 +10,19 @@ library(datasets)
 library(shiny)
 library(foreign)
 library(ggplot2)
+require(shinysky)
 
 
 data <- airquality[,c("Ozone","Solar.R","Wind","Temp")]
 Date.time <- paste(airquality$Day, airquality$Month, c(rep("1974",length(airquality$Day))))
 Date.time <- strptime(Date.time, "%d %m %Y")
 data$DateTime <- as.Date(as.character(strptime(Date.time, "%Y-%m-%d", tz="America/Los_Angeles")))
+data <- data[complete.cases(data),]
 
 
 shinyServer(
   function(input, output, session){
-    
+        
     observe({ 
       if (input$browse == 0) return()
       updateTextInput(session, "path", value = file.choose())
@@ -50,6 +52,13 @@ shinyServer(
       data_to_print$values <- dataset 
     })
     
+        
+    observe({
+      xmin<-as.numeric(min(data[,input$varx]))
+      xmax<-as.numeric(max(data[,input$varx]))
+      updateSliderInput(session, "slider",min=xmin-10, max=xmax+10, value=c(xmin,45))
+      
+    })
     
     #THIS IS THE ONE THAT WORKS
     #output$table<-renderDataTable(dataset, options=list(pageLength=5))
@@ -76,12 +85,43 @@ shinyServer(
 
   
     output$Plot2 <- renderPlot({
-      environment<-environment() 
-      ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()  
-
+      environment<-environment()   
+      xmin<-min(data[,input$varx])
+      xmax<-max(data[,input$varx])
+      smin<-input$slider[1]
+      smax<-input$slider[2]
       
+      #fillcolor <- ifelse(is.null(input$color_fill), "ffffff", input$color_fill)
+      #linecolor <- ifelse(is.null(input$color_line), "ffffff", input$color_line)
+      
+      if (input$fit_checkbox==TRUE) {
+        if (input$area_checkbox==TRUE){
+          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep="")) 
+        }
+        else{
+          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) 
+        }
+      }
+      else {
+        if (input$area_checkbox==TRUE){
+          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep=""))
+        }
+        else{
+          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax))))
+          
+        }
+      }
+    
     })
+    
+    output$fillcolor <- renderPrint({paste("#",input$color_fill,sep="")})
+    output$linecolor <- renderPrint({paste("#",input$color_line,sep="")})
 
-
+    output$fit_value <- renderPrint({input$fit_checkbox})
+    output$x_min <- renderPrint({min(data[,input$varx])})
+    output$x_max <- renderPrint({max(data[,input$varx])})
+    
+    output$s_max <- renderPrint({max(data[,input$varx])-(100-input$slider[2])})
+    output$s_min <- renderPrint({min(data[,input$varx])+input$slider[1]})
   }
 )
--- ui.R
+++ ui.R
@@ -1,30 +1,62 @@
+library(shiny)
 library(foreign)
 library(datasets)
-library(shiny)
 library(ggplot2)
-
-data <- airquality[,c("Ozone","Solar.R","Wind","Temp")]
-Date.time <- paste(airquality$Day, airquality$Month, c(rep("1974",length(airquality$Day))))
-Date.time <- strptime(Date.time, "%d %m %Y")
-data$DateTime <- as.character(strptime(Date.time, "%Y-%m-%d", tz="America/Los_Angeles"))
+require(shinysky)
 
 
-
-shinyUI(pageWithSidebar(
-  headerPanel("Weather App v1.0"),
-  sidebarPanel(
-    h4("Choose data"),
-    
-    selectInput("vary", "vary:", 
-                choices=c("Ozone","Solar.R","Wind","Temp")),
-    selectInput("varx", "varx:", 
-                choices=c("DateTime","Ozone","Solar.R","Wind","Temp")),
-    hr(),
-    helpText("Data from AT&T (1961) The World's Telephones.")
-  ),
-  mainPanel(
-    tabsetPanel(type="tabs",
-                tabPanel("Plot", plotOutput('Plot2')),
+shinyUI(
+    navbarPage("App v1.0",
+                tabPanel("Intro",helpText("WHATEVER. GO TO PLOT TAB.")),
+                tabPanel("Plot", 
+                         plotOutput('Plot2'),
+                         hr(),
+                         fluidRow(
+                           column(3,
+                                  h4("Choose data"),
+                                  
+                                  selectInput("vary", "vary:", 
+                                              choices=c("Ozone","Solar.R","Wind","Temp")),
+                                  selectInput("varx", "varx:", 
+                                              choices=c("DateTime","Ozone","Solar.R","Wind","Temp")),
+                                  hr(),
+                                  h4("Plot Var"),
+                                  checkboxInput("fit_checkbox", label = "Fit Line", value = FALSE),
+                                  checkboxInput("area_checkbox", label = "Plot Area", value = FALSE)
+                                  ),
+                           
+                           column(3, offset=1,
+                                  h4("Fit Line Params"),
+                                  h5("Line Color"),
+                                  jscolorInput("color_line_fit"),
+                                  h5("Fill Color"),
+                                  jscolorInput("color_fill_fit"),
+                                  
+                                  h4("Area Params"),
+                                  h5("Line Color"),
+                                  jscolorInput("color_line_area"),
+                                  h5("Fill Color"),
+                                  jscolorInput("color_fill_area"),
+                                  
+                                  br(),
+                                  sliderInput("slider", label = h4("X Axis Zoom Select"), min = 0, 
+                                              max = 100, value=c(0,100))
+                                  ),
+                           
+                           column(3,offset=1,
+                                  h4("X Min"),
+                                  verbatimTextOutput("s_min"),
+                                  h4("X Max"),
+                                  verbatimTextOutput("s_max"),
+                                  h4("X Minimum"),
+                                  verbatimTextOutput("x_min"),
+                                  h4("X Max"),
+                                  verbatimTextOutput("x_max")
+                           )
+                           
+                           )
+                         
+                         ),
                 tabPanel("Table",dataTableOutput('table')),
                 tabPanel("Input File", 
                          h4("Choose your data file"),
@@ -33,11 +65,5 @@ shinyUI(pageWithSidebar(
                          actionButton("upload", "Upload")),
                          dataTableOutput('table')
     )
-    #p('Names Read'),
-    #textOutput('text1')
-    #p('Time Req. Read'),
-    #textOutput('text2'),
-    #verbatimTextOutput('content')
-      
-    )
-))
+
+)
