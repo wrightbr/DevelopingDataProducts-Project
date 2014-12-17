bf07f7e35dc03a6105167c2b38e74e4a189e7e4e
manuelblancovalentin <wildsoul@hotmail.es>
2014-12-17 15:47:59 -0200

    v5.0
    Major Updates

--- server.r
+++ server.r
@@ -1,16 +1,9 @@
-Month <- c("None")
-Day <- c("None")
-Year <- c("None")
-Temp_F <- c("None")
-init_data_to_print <- data.frame(Month, Day, Year, Temp_F)
-dataset<-init_data_to_print
-
-
 library(datasets)
 library(shiny)
 library(foreign)
 library(ggplot2)
 require(shinysky)
+library(scales)
 
 
 data <- airquality[,c("Ozone","Solar.R","Wind","Temp")]
@@ -33,12 +26,6 @@ shinyServer(
       updateTextInput(session, "path", value = file.choose())
     })
     
-    data_to_print <- reactiveValues(values=init_data_to_print)
-    observe({
-      if (input$upload > 0)
-        return()
-      data_to_print$values <- init_data_to_print 
-    })
     
     
     observe({
@@ -53,75 +40,89 @@ shinyServer(
     })
     
         
-    observe({
-      xmin<-as.numeric(min(data[,input$varx]))
-      xmax<-as.numeric(max(data[,input$varx]))
-      updateSliderInput(session, "slider",min=xmin-10, max=xmax+10, value=c(xmin,45))
-      
-    })
+#     observe({
+#       xmin<-as.numeric(min(data[,input$varx]))
+#       xmax<-as.numeric(max(data[,input$varx]))
+#       updateSliderInput(session, "slider",min=xmin-10, max=xmax+10, value=c(xmin,45))
+#       
+#     })
     
-    #THIS IS THE ONE THAT WORKS
-    #output$table<-renderDataTable(dataset, options=list(pageLength=5))
             
     output$table <- renderDataTable(data, options=list(pageLength=5))
     
-    #output$text1 <- renderText(data)
-    #output$text2 <- renderText({as.numeric(data_to_print$values$EstimatedTimeRequired)})
-    #output$text3 <- renderText({
-#       input$goButton
-#       isolate(paste(input$text1, input$text2))
-#      if(input$goButton == 0) "You haven't pressed the button yet"
-#      else if(input$goButton == 1) "You pressed it once"
-#      else "OK quit pressing"
-#    })
-#     output$Plot1 <- renderPlot({
-#         plot(
-#            data$DateTime,
-#            data[,input$varx],
-#            main=input$varx,
-#            xlab = "XLAB",
-#            ylab = "YLAB")
-#     })
-
   
     output$Plot2 <- renderPlot({
       environment<-environment()   
       xmin<-min(data[,input$varx])
       xmax<-max(data[,input$varx])
       smin<-input$slider[1]
-      smax<-input$slider[2]
-      
-      #fillcolor <- ifelse(is.null(input$color_fill), "ffffff", input$color_fill)
-      #linecolor <- ifelse(is.null(input$color_line), "ffffff", input$color_line)
+      smax<-input$slider[2]  
+            
       
       if (input$fit_checkbox==TRUE) {
         if (input$area_checkbox==TRUE){
-          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep="")) 
+          if (input$varx=="DateTime"){
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_date(limits= as.Date(c(xmin+smin,xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) + geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep="")) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep=""))
+          }
+          else{
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_continuous(limits= c(as.numeric(xmin+smin),as.numeric(xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) + geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep="")) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep=""))
+          } 
         }
         else{
-          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) 
+          if (input$varx=="DateTime"){
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) + geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))  
+          }
+          else {
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_continuous(limits= c(as.numeric(xmin+smin),as.numeric(xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) + geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))
+          } 
         }
       }
       else {
         if (input$area_checkbox==TRUE){
-          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep=""))
+          if (input$varx=="DateTime"){
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep=""))
+          }
+          else{
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_continuous(limits= c(as.numeric(xmin+smin),as.numeric(xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep=""))
+          }
         }
         else{
-          ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)+geom_point()+scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax))))
-          
+          if (input$varx=="DateTime"){
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_date(limits=as.Date(c(xmin+smin,xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) 
+          }
+          else{
+            ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment) + geom_point() + scale_x_continuous(limits= c(as.numeric(xmin+smin),as.numeric(xmax-(100-smax)))) + xlab(as.character(input$varx)) + ylab(as.character(input$vary)) 
+          }
         }
       }
     
     })
     
+    output$LAB_DATE <- renderText({
+      
+      if (input$varx=="DateTime") TRUE
+      else FALSE
+      
+    })
+    
+    
     output$fillcolor <- renderPrint({paste("#",input$color_fill,sep="")})
     output$linecolor <- renderPrint({paste("#",input$color_line,sep="")})
 
     output$fit_value <- renderPrint({input$fit_checkbox})
-    output$x_min <- renderPrint({min(data[,input$varx])})
-    output$x_max <- renderPrint({max(data[,input$varx])})
+    output$x_min <- renderText({as.character(min(data[,input$varx]))})
+    output$x_max <- renderText({as.character(max(data[,input$varx]))})
+    output$y_min <- renderText({as.character(min(data[,input$vary]))})
+    output$y_max <- renderText({as.character(max(data[,input$vary]))})
     
     output$s_max <- renderPrint({max(data[,input$varx])-(100-input$slider[2])})
     output$s_min <- renderPrint({min(data[,input$varx])+input$slider[1]})
+    
+    output$var_x <- renderText({as.character(input$varx)})
+    output$var_y <- renderText({as.character(input$vary)})
+    #output$var_x_min <- renderText({paste(as.character(input$varx),"min. value")})
+    #output$var_x_max <- renderText({paste(as.character(input$varx),"max. value")})
+    #output$var_y_min <- renderText({paste(as.character(input$vary),"min. value")})
+    #output$var_y_max <- renderText({paste(as.character(input$vary),"max. value")})
   }
 )
--- ui.R
+++ ui.R
@@ -3,16 +3,18 @@ library(foreign)
 library(datasets)
 library(ggplot2)
 require(shinysky)
+library(scales)
 
 
 shinyUI(
     navbarPage("App v1.0",
                 tabPanel("Intro",helpText("WHATEVER. GO TO PLOT TAB.")),
                 tabPanel("Plot", 
-                         plotOutput('Plot2'),
+                         h4("Plot of your Data", align='center'),
+                         wellPanel(plotOutput('Plot2')),
                          hr(),
                          fluidRow(
-                           column(3,
+                           column(3,wellPanel(
                                   h4("Choose data"),
                                   
                                   selectInput("vary", "vary:", 
@@ -22,10 +24,40 @@ shinyUI(
                                   hr(),
                                   h4("Plot Var"),
                                   checkboxInput("fit_checkbox", label = "Fit Line", value = FALSE),
-                                  checkboxInput("area_checkbox", label = "Plot Area", value = FALSE)
-                                  ),
+                                  checkboxInput("area_checkbox", label = "Plot Area", value = FALSE),
+                                  verbatimTextOutput("LAB_DATE")
+                                  )),
                            
-                           column(3, offset=1,
+                           column(3,wellPanel(
+                             h4(textOutput('var_x')),
+                             fluidRow(
+                               column(6,
+                                      h5('min. value'),
+                                      #h5(textOutput('var_x_min')),
+                                      verbatimTextOutput("x_min")
+                                      #textOutput("x_min")
+                                      ),
+                               column(6,
+                                      h5('max. value'),
+                                      #h5(textOutput('var_x_max')),
+                                      verbatimTextOutput("x_max")
+                                      )
+                               ),
+                             h4(textOutput('var_y')),
+                             fluidRow(
+                               column(6,
+                                      h5('min. value'),
+                                      verbatimTextOutput("y_min")
+                               ),
+                               column(6,
+                                      h5('max. value'),
+                                      verbatimTextOutput("y_max")
+                               )
+                             )
+
+                             )),                           
+                           
+                           column(3, wellPanel(
                                   h4("Fit Line Params"),
                                   h5("Line Color"),
                                   jscolorInput("color_line_fit"),
@@ -36,23 +68,26 @@ shinyUI(
                                   h5("Line Color"),
                                   jscolorInput("color_line_area"),
                                   h5("Fill Color"),
-                                  jscolorInput("color_fill_area"),
+                                  jscolorInput("color_fill_area")
                                   
-                                  br(),
+                           )),
+                           column(3, wellPanel(
                                   sliderInput("slider", label = h4("X Axis Zoom Select"), min = 0, 
-                                              max = 100, value=c(0,100))
-                                  ),
-                           
-                           column(3,offset=1,
-                                  h4("X Min"),
-                                  verbatimTextOutput("s_min"),
+                                              max = 100, value=c(0,100), format="## Days"),
+                                  br(),
+                                  h4(verbatimTextOutput("s_min")),
                                   h4("X Max"),
-                                  verbatimTextOutput("s_max"),
-                                  h4("X Minimum"),
-                                  verbatimTextOutput("x_min"),
-                                  h4("X Max"),
-                                  verbatimTextOutput("x_max")
-                           )
+                                  verbatimTextOutput("s_max")
+ 
+#                                   h5(textOutput('var_x_min')),
+#                                   verbatimTextOutput("x_min"),
+#                                   h5(textOutput('var_x_max')),
+#                                   verbatimTextOutput("x_max"),
+#                                   h5(textOutput('var_y_min')),
+#                                   verbatimTextOutput("y_min"),
+#                                   h5(textOutput('var_y_max')),
+#                                   verbatimTextOutput("y_max")
+                           ))
                            
                            )
                          
