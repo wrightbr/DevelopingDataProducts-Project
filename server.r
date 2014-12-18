37b9e960217c1fb0a813df2e83d75ba34c5a839b
manuelblancovalentin <wildsoul@hotmail.es>
2014-12-18 18:28:51 -0200

    v8.0
    Major Updates

--- server.r
+++ server.r
@@ -5,6 +5,71 @@ library(ggplot2)
 require(shinysky)
 library(scales)
 
+Plotfunction <- function(varx, vary, slider_x, slider_y, points_checkbox, line_checkbox, color_lines, area_checkbox, color_fill_area, color_line_area, fit_checkbox, color_fill_fit, color_line_fit, session){
+  
+  environment<-environment()   
+  
+  xmin<-min(data[,varx])
+  xmax<-max(data[,varx])
+  
+  ymin<-min(data[,vary])
+  ymax<-max(data[,vary])
+  
+  smin_x<-slider_x[1]
+  smax_x<-slider_x[2]  
+  
+  smin_y<-slider_y[1]
+  smax_y<-slider_y[2]
+  
+  #Basic Plot Params
+  p <- ggplot(aes(data[,varx], data[,vary]),data = data, environment=environment)
+  
+  lab_x <- xlab(as.character(varx))
+  lab_y <- ylab(as.character(vary))
+  
+  sy <- coord_cartesian(ylim = c(as.numeric(ymin+(smin_y*(ymax-ymin)/100)),as.numeric(ymax-((100-smax_y)*(ymax-ymin))/100)))
+  
+  p <- p + lab_x + lab_y + sy
+  
+  
+  #Check if need for date X scale or not
+  if (varx=="DateTime"){
+    p <- p + scale_x_date(limits=as.Date(c(xmin+(smin_x*(xmax-xmin)/100),xmax-((100-smax_x)*(xmax-xmin))/100)))
+  }
+  else{
+    p <- p + scale_x_continuous(limits= c(as.numeric(xmin+(smin_x*(xmax-xmin)/100)),as.numeric(xmax-((100-smax_x)*(xmax-xmin))/100)))
+  }
+  
+  #Check if points have to be added
+  if (points_checkbox==TRUE){
+    if (as.character(color_lines) != "FFFFFF"){
+      p <- p + geom_point(fill=paste("#",color_points,sep=""), color=paste("#",color_points,sep=""))
+    }
+    else{
+      p <- p + geom_point(fill=paste("#","000000",sep=""), color=paste("#","000000",sep=""))
+    }
+    
+  }
+  
+  #Check if line has to be added
+  if (line_checkbox==TRUE){
+    p <- p + geom_line(fill=paste("#",color_lines,sep=""), color=paste("#",color_lines,sep=""))
+  }
+  
+  #Check if area has to be added
+  if (area_checkbox==TRUE){
+    p <- p + geom_area(fill=paste("#",color_fill_area,sep=""), color=paste("#",color_line_area,sep="")) 
+  }
+  
+  #Check if fit line has to be added
+  if (fit_checkbox==TRUE) {
+    p <- p + geom_smooth(fill=paste("#",color_fill_fit,sep=""), color=paste("#",color_line_fit,sep=""))
+  }
+  
+  return(p)
+
+}
+
 
 data <- airquality[,c("Ozone","Solar.R","Wind","Temp")]
 Date.time <- paste(airquality$Day, airquality$Month, c(rep("1974",length(airquality$Day))))
@@ -16,6 +81,8 @@ data <- data[complete.cases(data),]
 shinyServer(
   function(input, output, session){
         
+ 
+##Download data table    
     observe({ 
       if (input$browse == 0) return()
       updateTextInput(session, "path", value = choose.dir())
@@ -27,9 +94,30 @@ shinyServer(
         return()
       dir_0 <- getwd()
       setwd(input$path)
-
-      
       write.table(data[, input$show_vars, drop=FALSE], paste(input$file_name,input$ext,sep="."), sep=as.character(input$separ))
+      setwd(dir_0)
+    })
+
+    observe({ 
+      if (input$browse == 0) return()
+      updateTextInput(session, "path", value = choose.dir())
+    })    
+    
+
+    
+
+#Download printed plot
+    observe({
+      if (input$download_img == 0)
+        return()
+      dir_0 <- getwd()
+      setwd(input$path_img)
+      png(as.character(paste(input$file_name_img,"png",sep="."), width = 480, height = 720))
+      print(Plotfunction(input$varx, input$vary, input$slider_x, input$slider_y, 
+                   input$points_checkbox, input$line_checkbox, input$color_lines, 
+                   input$area_checkbox, input$color_fill_area, input$color_line_area, 
+                   input$fit_checkbox, input$color_fill_fit, input$color_line_fit, session))
+      dev.off()
       setwd(dir_0)
     })
     
@@ -38,7 +126,22 @@ shinyServer(
         updateCheckboxInput(session, "points_checkbox", value = TRUE)
       }
     }) 
-      
+
+    
+    observe({ 
+      if (input$browse_img == 0) return()
+      updateTextInput(session, "path_img", value = choose.dir())
+    })  
+ 
+
+#Safety function
+    observe({
+      if ((input$points_checkbox==FALSE) & (input$line_checkbox==FALSE) & (input$area_checkbox==FALSE) & (input$fit_checkbox==FALSE)) {
+        updateCheckboxInput(session, "points_checkbox", value = TRUE)
+      }
+    }) 
+
+
     
     output$table <- renderDataTable({
       data[, input$show_vars, drop=FALSE] 
@@ -46,70 +149,88 @@ shinyServer(
     
   
     output$MainPlot <- renderPlot({
-      environment<-environment()   
       
-      xmin<-min(data[,input$varx])
-      xmax<-max(data[,input$varx])
-      
-      ymin<-min(data[,input$vary])
-      ymax<-max(data[,input$vary])
-      
-      smin_x<-input$slider_x[1]
-      smax_x<-input$slider_x[2]  
-      
-      smin_y<-input$slider_y[1]
-      smax_y<-input$slider_y[2]
-          
-      #Basic Plot Params
-      p <- ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)
-      
-      lab_x <- xlab(as.character(input$varx))
-      lab_y <- ylab(as.character(input$vary))
-      
-      sy <- coord_cartesian(ylim = c(as.numeric(ymin+(smin_y*(ymax-ymin)/100)),as.numeric(ymax-((100-smax_y)*(ymax-ymin))/100)))
-      
-      p <- p + lab_x + lab_y + sy
+      Plotfunction(input$varx, input$vary, input$slider_x, input$slider_y, 
+                 input$points_checkbox, input$line_checkbox, input$color_lines, 
+                 input$area_checkbox, input$color_fill_area, input$color_line_area, 
+                 input$fit_checkbox, input$color_fill_fit, input$color_line_fit, session)
+                
       
       
-      #Check if need for date X scale or not
-      if (input$varx=="DateTime"){
-        p <- p + scale_x_date(limits=as.Date(c(xmin+(smin_x*(xmax-xmin)/100),xmax-((100-smax_x)*(xmax-xmin))/100)))
-      }
-      else{
-        p <- p + scale_x_continuous(limits= c(as.numeric(xmin+(smin_x*(xmax-xmin)/100)),as.numeric(xmax-((100-smax_x)*(xmax-xmin))/100)))
-      }
-      
-      #Check if points have to be added
-      if (input$points_checkbox==TRUE){
-          if (as.character(input$color_lines) != "FFFFFF"){
-            p <- p + geom_point(fill=paste("#",input$color_points,sep=""), color=paste("#",input$color_points,sep=""))
-          }
-          else{
-            p <- p + geom_point(fill=paste("#","000000",sep=""), color=paste("#","000000",sep=""))
-          }
-
-      }
-      
-      #Check if line has to be added
-      if (input$line_checkbox==TRUE){
-        p <- p + geom_line(fill=paste("#",input$color_lines,sep=""), color=paste("#",input$color_lines,sep=""))
-      }
-      
-      #Check if area has to be added
-      if (input$area_checkbox==TRUE){
-        p <- p + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep="")) 
-      }
-      
-      #Check if fit line has to be added
-      if (input$fit_checkbox==TRUE) {
-        p <- p + geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))
-      }
-
-      return(p)
-    
+#       environment<-environment()   
+#       
+#       xmin<-min(data[,input$varx])
+#       xmax<-max(data[,input$varx])
+#       
+#       ymin<-min(data[,input$vary])
+#       ymax<-max(data[,input$vary])
+#       
+#       smin_x<-input$slider_x[1]
+#       smax_x<-input$slider_x[2]  
+#       
+#       smin_y<-input$slider_y[1]
+#       smax_y<-input$slider_y[2]
+#           
+#       #Basic Plot Params
+#       p <- ggplot(aes(data[,input$varx], data[,input$vary]),data = data, environment=environment)
+#       
+#       lab_x <- xlab(as.character(input$varx))
+#       lab_y <- ylab(as.character(input$vary))
+#       
+#       sy <- coord_cartesian(ylim = c(as.numeric(ymin+(smin_y*(ymax-ymin)/100)),as.numeric(ymax-((100-smax_y)*(ymax-ymin))/100)))
+#       
+#       p <- p + lab_x + lab_y + sy
+#       
+#       
+#       #Check if need for date X scale or not
+#       if (input$varx=="DateTime"){
+#         p <- p + scale_x_date(limits=as.Date(c(xmin+(smin_x*(xmax-xmin)/100),xmax-((100-smax_x)*(xmax-xmin))/100)))
+#       }
+#       else{
+#         p <- p + scale_x_continuous(limits= c(as.numeric(xmin+(smin_x*(xmax-xmin)/100)),as.numeric(xmax-((100-smax_x)*(xmax-xmin))/100)))
+#       }
+#       
+#       #Check if points have to be added
+#       if (input$points_checkbox==TRUE){
+#           if (as.character(input$color_lines) != "FFFFFF"){
+#             p <- p + geom_point(fill=paste("#",input$color_points,sep=""), color=paste("#",input$color_points,sep=""))
+#           }
+#           else{
+#             p <- p + geom_point(fill=paste("#","000000",sep=""), color=paste("#","000000",sep=""))
+#           }
+# 
+#       }
+#       
+#       #Check if line has to be added
+#       if (input$line_checkbox==TRUE){
+#         p <- p + geom_line(fill=paste("#",input$color_lines,sep=""), color=paste("#",input$color_lines,sep=""))
+#       }
+#       
+#       #Check if area has to be added
+#       if (input$area_checkbox==TRUE){
+#         p <- p + geom_area(fill=paste("#",input$color_fill_area,sep=""), color=paste("#",input$color_line_area,sep="")) 
+#       }
+#       
+#       #Check if fit line has to be added
+#       if (input$fit_checkbox==TRUE) {
+#         p <- p + geom_smooth(fill=paste("#",input$color_fill_fit,sep=""), color=paste("#",input$color_line_fit,sep=""))
+#       }
+# 
+#       return(p)
+#     
     })
     
       
+    output$x_avg<- renderText({
+      if (input$varx=="DateTime"){
+        as.character(mean(data[,input$varx]))
+      }
+      else{
+        as.character(round(mean(data[,input$varx])),0)
+      }
+    })
+    
+    output$y_avg<- renderText({as.character(round(mean(data[,input$vary])),2)})
     
     output$fillcolor <- renderPrint({paste("#",input$color_fill,sep="")})
     output$linecolor <- renderPrint({paste("#",input$color_line,sep="")})
--- ui.R
+++ ui.R
@@ -13,29 +13,131 @@ data <- data[complete.cases(data),]
 
 shinyUI(
     navbarPage("AirQuality Data Analysis App v6.0",
-                tabPanel("About the App",helpText("With this Shiny App you'll be able to explore the 'Airquality' dataset, from 'R Datasets' package.")),
-                tabPanel("Plot", 
+                tabPanel("About the App",
+                  wellPanel(
+                    h4("Welcome!"),
+                    helpText("My name is Manu B. and I created this Shiny App for the 'Developing Data Products' course in coursera. 
+                             You may find it simple or boring, but I consider it good enough for the available time we got. 
+                             Using this App you'll be able to explore the 'Airquality' dataset, from 'R Datasets' package, which
+                             you may find on the link below:"),
+                    helpText(a("Air Quality Datasets Reference",href="http://stat.ethz.ch/R-manual/R-patched/library/datasets/html/airquality.html"))
+                    #,
+                  ),
+                  #hr(),
+                  wellPanel(
+                    h4("About the airquality dataset"),
+                    helpText("This dataset provides a total of 153 measurements of Ozone and Solar Radiation levels, wind velocity (in mph),
+                             and Temperature (in Fahrenheit), taken from 1st May 1974 to 30th Sep of the same year at La Guardia Airport, NYC.")
+                    #,
+                  ),
+                    #hr(),
+                  wellPanel(
+                    h4("App functionalities"),
+                    helpText("With this app you'll be able to, basically:"),
+                    tags$ul(
+                      tags$li(h5("Plot the data:"),
+                        tags$ul(
+                          tags$li(
+                            "Plot X vs Y plots with any of the variables contained."
+                            ),
+                          tags$li(
+                            "See the maximum, minimum and average values for selected variables on plot."
+                            ),
+                          tags$li(
+                            "Zoom at the X and Y axis to obtain a focused view of data."
+                          ),
+                          tags$li(
+                            "Select the plot symbols (points, lines, area)."
+                          ),
+                          tags$li(
+                            "Add, if desired, a fit line to the data plotted."
+                          ),
+                          tags$li(
+                            "Change the colors for each of the lines, areas or points plotted."
+                          )
+                          )
+                      ),
+                        
+                      tags$li(h5("See the data:"),
+                              tags$ul(
+                                tags$li(
+                                  "See the whole datatable."
+                                ),
+                                tags$li(
+                                  "Select the variables you want the table to show."
+                                ),
+                                tags$li(
+                                  "Sort the data by different aspects (i.e., lower to higher temperature)."
+                                ),
+                                tags$li(
+                                  "Search for specific data values for any variable."
+                                )
+                              )     
+                        ),
+                      
+                      tags$li(h5("Download the data:"),
+                              tags$ul(
+                                tags$li(
+                                  "Download the table (according to selected variables and choices)."
+                                ),
+                                tags$li(
+                                  "Download and Save in your computer your custom plot."
+                                )
+                              )     
+                      )
+                      
+                    )
+                  )
+                ),
+               
+               tabPanel("How to use this App",
+                        wellPanel(
+                            h4("How to use the Plot tab functions:"),
+                            helpText("A")
+                          ),
+                        wellPanel(
+                          h4("How to use the Table tab functions:"),
+                          helpText("B")
+                          )
+                  
+               ),
+               
+               tabPanel("Plot", 
                          h4("Plot of your Data", align='center'),
-                         wellPanel(plotOutput('MainPlot')),
+                         wellPanel(
+                           fluidRow(
+                             column(2,
+                                    h4("Choose data"),
+                                    
+                                    selectInput("vary", "Y Variable:", 
+                                                choices=c("Ozone","Solar.R","Wind","Temp")),
+                                    selectInput("varx", "X Variable:", 
+                                                choices=c("DateTime","Ozone","Solar.R","Wind","Temp")),
+                                    hr(),
+                                    h4("Plot Var"),
+                                    checkboxInput("points_checkbox", label = "Plot Points", value = TRUE),
+                                    checkboxInput("line_checkbox", label = "Plot Line", value = FALSE),
+                                    checkboxInput("fit_checkbox", label = "Add Fit Line", value = FALSE),
+                                    checkboxInput("area_checkbox", label = "Plot Data Area", value = FALSE)
+                                    
+                             ),
+                             column(10,
+                                    plotOutput('MainPlot'))
+                           )
+                           ),
                          hr(),
                          fluidRow(
                            column(3,wellPanel(
-                                  h4("Choose data"),
-                                  
-                                  selectInput("vary", "Y Variable:", 
-                                              choices=c("Ozone","Solar.R","Wind","Temp")),
-                                  selectInput("varx", "X Variable:", 
-                                              choices=c("DateTime","Ozone","Solar.R","Wind","Temp")),
-                                  hr(),
-                                  h4("Plot Var"),
-                                  checkboxInput("points_checkbox", label = "Plot Points", value = TRUE),
-                                  checkboxInput("line_checkbox", label = "Plot Line", value = FALSE),
-                                  checkboxInput("fit_checkbox", label = "Add Fit Line", value = FALSE),
-                                  checkboxInput("area_checkbox", label = "Plot Data Area", value = FALSE)
-                                  )),
-                           
-                           column(3,wellPanel(
                              h4(textOutput('var_x')),
+                             fluidRow(
+                               column(5,
+                                      h5('Average')
+                                      ),
+                               column(6,
+                                      verbatimTextOutput('x_avg')
+                                      )
+                               ),
+
                              fluidRow(
                                column(6,
                                       h5('min. value'),
@@ -46,7 +148,17 @@ shinyUI(
                                       verbatimTextOutput("x_max")
                                       )
                                ),
+
                              h4(textOutput('var_y')),
+                             fluidRow(
+                               column(5,
+                                      h5('Average')
+                               ),
+                               column(6,
+                                      verbatimTextOutput('y_avg')
+                               )
+                             ),
+                          
                              fluidRow(
                                column(6,
                                       h5('min. value'),
@@ -125,6 +237,17 @@ shinyUI(
                                     )
                                   ) 
                                   
+                           )),
+                           
+                           column(3,wellPanel(
+                             helpText("Hey"),
+                             h4("Save this plot as png"),
+                             textInput("path_img", "Choose the destination folder:"),
+                             actionButton("browse_img", "Browse"),
+                             textInput("file_name_img", "Name the file:"),
+                             br(),
+                             actionButton("download_img", "Save")
+                             
                            ))
                            
                            )
@@ -134,6 +257,7 @@ shinyUI(
                tabPanel("Table", 
                          sidebarLayout(
                            sidebarPanel(
+                             h4("Select variables to display:"),
                              checkboxGroupInput('show_vars','Columns in airquality dataset to show:',
                                                 names(data), selected = names(data)),
                              hr(),
@@ -157,14 +281,22 @@ shinyUI(
                           
                         )
     
-                  )
+                  ),
+               
+               tabPanel("Reference articles and code",
+                        wellPanel(
+                          h4("Github Repo:"),
+                          helpText("The main code and files you can find on my Github Repository for this project (see link below):"),
+                          helpText(a("AirQuality Data Analisys App 6.0 Github Repo:",href="https://github.com/manuelblancovalentin/DevelopingDataProducts-Project/"))
+                        ),
+                        wellPanel(
+                          h4("How to use the Table tab functions:"),
+                          helpText("B")
+                        )
+                        
+               )
      
-#                 tabPanel("Download Data File", 
-#                          h4("Choose the destination of your data file:"),
-#                          textInput("path", "Path:"),
-#                          actionButton("browse", "Browse"),
-#                          actionButton("download", "Download")
-#                 )
+
     )
 
 )
